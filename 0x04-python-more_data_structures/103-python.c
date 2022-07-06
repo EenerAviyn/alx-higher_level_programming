6cb6e9d11e8f5d7cd663b95b18e0a11c56f18f02
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2022-07-06 21:39:35 +0300

    103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -12,7 +12,7 @@ void print_python_bytes(PyObject *p)
 	size_t i, length, size;
 	char *str;
 
-	printf("[.]bytes object info\n");
+	printf("[.] bytes object info\n");
 	if (strcmp(p->ob_type->tp_name, "bytes"))
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
@@ -25,7 +25,7 @@ void print_python_bytes(PyObject *p)
 	printf("  trying string: %s\n", str);
 	printf("  first %lu bytes: ", length);
 	for (i = 0; i < len; i++)
-		printf("02hhx%s", str[i], i + 1 < len ? " " : "");
+		printf("%02hhx%s", str[i], i + 1 < len ? " " : "");
 	printf("\n");
 }
 
@@ -44,9 +44,8 @@ void print_python_list(PyObject *p)
 	for (i = 0; i < ((PyVarObject *)p)->ob_size; i++)
 	{
 		printf("Element %d: %s\n", i,
-				((PyListObject *)p)->ob_item[i]->ob_type->tp_name);
+			((PyListObject *)p)->ob_item[i]->ob_type->tp_name);
 		if (!strcmp(((PyListObject *)p)->ob_item[i]->ob_type->tp_name, "bytes"))
 			print_python_bytes(((PyListObject *)p)->ob_item[i]);
 	}
 }
-
