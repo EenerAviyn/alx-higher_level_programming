c16893bc1db9ff2dcea6657399978c8ff7cdee13
EenerAviyn <mulelarenee18@gmail.com>
2022-07-19 11:38:29 -0700

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square module - assigns size, checks type, value"""
+
+
+class Square:
+    """Defines square with private instance attribute size"""
+
+    def __init__(self, size=0):
+        """assigns size of square and checks type and value"""
+
+        if not isinstance(size, int):
+        raise TypeError("size must be an interger")
+        if size < 0:
+        raise ValueError("size mmust be >= 0")
+        self.__size = size
