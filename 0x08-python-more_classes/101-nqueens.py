2d95c40a8157f4a9089849832dcc19132c2d0fce
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2022-07-26 00:25:14 +0300

    101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-nqueens backtracking program to print the coordinates of n queens
+nqueens backtracking program to print coordinates of n queens
 on an nxn grid such that they are all in non-attacking positions
 """
 
@@ -25,14 +25,14 @@ if __name__ == "__main__":
         a.append([i, None])
 
     def already_exists(y):
-        """check that a queen does not already exist in that y value"""
+        """check that queen does not already exist in that y value"""
         for x in range(n):
             if y == a[x][1]:
                 return True
         return False
 
     def reject(x, y):
-        """determines whether or not to reject the solution"""
+        """determines whether or not to reject solution"""
         if (already_exists(y)):
             return False
         i = 0
@@ -43,7 +43,7 @@ if __name__ == "__main__":
         return True
 
     def clear_a(x):
-        """clears the answers from the point of failure on"""
+        """clears answers from point of failure on"""
         for i in range(x, n):
             a[i][1] = None
 
@@ -58,5 +58,5 @@ if __name__ == "__main__":
                 else:
                     nqueens(x + 1)  # moves on to next x value to continue
 
-    # start the recursive process at x = 0
+    # start recursive process at x = 0
     nqueens(0)
