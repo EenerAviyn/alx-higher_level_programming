9c9582ca7406a1e7890d4463e5b7d0fa489dad60
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2022-08-02 14:36:29 +0300

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module for script that adds all arguments to Python list"""
+"""Module for a script that adds all arguments to a Python list"""
 import json
 from sys import argv
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
@@ -9,7 +9,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 file_name = "add_item.json"
 
 # can also use os.path.exists(file) for try and except
-# import os.path
+# be sure to import os.path
 try:
     json_list = load_from_json_file(file_name)
 except:
