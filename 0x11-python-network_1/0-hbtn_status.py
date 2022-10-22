88eda4dc2420db88937ced42a5b59396da0072d9
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2022-10-22 18:06:37 +0300

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """A script that:
--fetches https://alx-intranet.hbtn.io/status
--uses urllib package
+- fetches https://alx-intranet.hbtn.io/status
+- uses urllib package
 """
 
 
 if __name__ == '__main__':
     import urllib.request
-  
+
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
         content = res.read()
         print("Body response:")
