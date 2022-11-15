0fe392e37cdc0dbbbec590086a171c4a30eaa6f2
Renée Mulela <42552984+EenerAviyn@users.noreply.github.com>
2022-11-16 00:28:00 +0300

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+// prints number of movies where "Wedge Antilles' is present
+
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+  if (error) throw new Error(error);
+  let count = 0;
+  JSON.parse(body).results.forEach(film => {
+    film.characters.forEach(character => {
+      const res = character.includes('/18/');
+      count += (res ? 1 : 0);
+    });
+  });
+  console.log(count);
+});
